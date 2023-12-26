e4df22329052f5675825ffd100b56705747221e6
juujacometti <julyajacometty@gmail.com>
2023-12-26 16:17:36 -0300

    Exercícios finalizados

--- 19-24/src/exercicio24_VetorXYZ.java
+++ 19-24/src/exercicio24_VetorXYZ.java
@@ -8,18 +8,42 @@ public class exercicio24_VetorXYZ {
 
 // Vetor:
 
-        int[] vetX = new int[] {50, 70, 30, 60, 100};
-        int[] vetY = new int[] {90, 50, 100, 30, 80};
+        int[] vetX = new int[]{50, 70, 30, 60, 100};
+        int[] vetY = new int[]{90, 50, 100, 30, 80};
         int[] vetZ = new int[3];
 
-// Informação p usuário:
+// Looping de informação p usuário:
 
-        System.out.println("1º Quadro de números: " + vetX + "\n2º Quadro de números: " + vetY);
-
-// Looping:
-
-        for(int i = 0; i < 5; i++) {
-
+        System.out.println("1º Quadro de números: ");
+        for(int i = 0; i < vetX.length; i++) {
+            System.out.println(vetX[i]);
         }
+
+        System.out.println("\n2º Quadro de números: ");
+        for(int i = 0; i < vetY.length; i++) {
+            System.out.println(vetY[i]);
+        }
+
+// Looping Z:
+
+        int contador = 0;
+
+        for(int i = 0; i < vetX.length; i++) {
+
+            for (int b = 0; b < vetY.length; b++) {
+
+                if (vetX[i] == vetY[b]) {
+                    vetZ[contador++] = vetX[i];
+                }
+            }
+        }
+
+// Execução:
+
+        System.out.println("\nOs números que aparecem simultaneamente nos dois quadros de números são: ");
+        for(int i = 0; i < vetZ.length; i++) {
+            System.out.println(vetZ[i]);
+        }
+
     }
 }
