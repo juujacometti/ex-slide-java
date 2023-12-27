19acee4b657b3fd08366bb438af428fcb4f1b399
juujacometti <julyajacometty@gmail.com>
2023-12-27 13:17:37 -0300

    exercicios

---
+++ 25-29/_25-29/src/exercicio25_mtz3x3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class exercicio25_mtz3x3 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+// OBJETIVO: Preencha uma matriz de 3x3 com números inteiros informados pelo usuário e apresente os valores armazenados como uma tabela 3x3.
+
+// Vetor:
+
+        int[][] numero = new int[3][3];
+
+// Looping:
+
+        for(int linha = 0; linha < numero.length; linha++) {
+
+            for(int coluna = 0; coluna < numero.length; coluna++) {
+                System.out.println("Insira aqui os números que você deseja inserir na tabela: ");
+                numero[linha][coluna] = leia.nextInt();
+
+            }
+        }
+
+// Execução:
+
+        System.out.println("Esses são os números escohidos: ");
+        for(int linha = 0; linha < numero.length; linha++) {
+            for (int coluna = 0; coluna < numero.length; coluna++) {
+                System.out.print(numero[linha][coluna] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
---
+++ 25-29/_25-29/src/exercicio26_mtz7x2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class exercicio26_mtz7x2 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+// OBJETIVO: Preencha uma matriz de 7x2 com valores referentes a temperaturas mínimas e máximas de uma semana e apresente os valores como solicitado no slide.
+
+// Vetor: 
+
+    }
+}
